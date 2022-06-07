f9ae3b26cda08abe05c98930524ff9086a2197a1
Joep-vink <78424962+Joep-vink@users.noreply.github.com>
2022-06-07 09:30:20 +0200

    j

--- ShortBurn/Assets/Joep/Scripts/PlayerRecording/InputRecorder.cs
+++ ShortBurn/Assets/Joep/Scripts/PlayerRecording/InputRecorder.cs
@@ -41,19 +41,17 @@ public class InputRecorder : MonoBehaviour
             if (playerInputRecord[i].TimeStamp < _timeStamp)
                 continue;
 
-            /*deltaTime = Mathf.Abs(deltaTime - _timeStamp);
+            deltaTime = Mathf.Abs(deltaTime - _timeStamp);
             float _lerpTime = _timeStamp / deltaTime;
 
             //Debug.Log("Lerp Time: " + _lerpTime + " Delta: " + deltaTime);
 
-            deltaTime = _timeStamp;*/
+            deltaTime = _timeStamp;
 
-            return playerInputRecord[i];
-            
-            /*if ((int)_lerpTime < playerInputRecord.Count - 1)
+            if ((int)_lerpTime < playerInputRecord.Count - 1)
                 return playerInputRecord[(int)_lerpTime];
             else
-                return playerInputRecord[playerInputRecord.Count - 1];*/
+                return playerInputRecord[playerInputRecord.Count - 1];
         }
 
         return playerInputRecord[playerInputRecord.Count - 1];
