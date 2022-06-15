2fbd33e811465ca102dce4277e8b517680b669ce
NnelisS <78425965+NnelisS@users.noreply.github.com>
2022-06-15 11:52:59 +0200

    Button And Pickup

---
+++ ShortBurn/Assets/Niels/Scripts/PlateDown.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateDown : MonoBehaviour
+{
+    private Vector3 upPos;
+    private Vector3 downPos;
+
+    private bool On = false;
+
+    void Start()
+    {
+        upPos = transform.position;
+        downPos = transform.position -= new Vector3(0, 0.1f, 0);
+    }
+
+    void Update()
+    {
+        if (On)
+            transform.position = Vector3.Lerp(transform.position, downPos, 1 * Time.deltaTime);
+        else
+            transform.position = Vector3.Lerp(transform.position, upPos, 1 * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Clone") || other.gameObject.CompareTag("CubeNormal"))
+            On = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Clone") || other.gameObject.CompareTag("CubeNormal"))
+            On = false;
+    }
+}
