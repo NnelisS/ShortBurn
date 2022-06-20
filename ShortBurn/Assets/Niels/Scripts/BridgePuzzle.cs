aed6a00ff70f491ee1f5071218a0207d2299b9a5
NnelisS <78425965+NnelisS@users.noreply.github.com>
2022-06-20 10:41:12 +0200

    Bridge puzzle and audio

--- ShortBurn/Assets/Niels/Scripts/BridgePuzzle.cs
+++ ShortBurn/Assets/Niels/Scripts/BridgePuzzle.cs
@@ -8,54 +8,29 @@ public class BridgePuzzle : MonoBehaviour
     public Transform Bridge;
     public Transform GoTo;
     public Transform GoToBack;
-    public bool ClonePuzzle = false;
 
-    private float timer = 2;
-    private CinemachineVirtualCamera vCam;
+    public bool activated = false;
+    public bool activateMusic = false;
 
-    private bool activated = false;
-
-    private void Start()
-    {
-        vCam = FindObjectOfType<CinemachineVirtualCamera>();
-    }
+    public AudioSource Ending;
 
     private void Update()
     {
         if (activated)
+            Bridge.transform.localPosition = Vector3.MoveTowards(Bridge.transform.localPosition, GoTo.transform.localPosition, 0.3f * Time.deltaTime);
+
+        if (activateMusic)
         {
-            if (timer > 0.1f && ClonePuzzle == true)
-            {
-                vCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 2f;
-                vCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = 25f;
-            }
-            Bridge.transform.localPosition = Vector3.MoveTowards(Bridge.transform.localPosition, GoTo.transform.localPosition, 1 * Time.deltaTime);
-            
-            timer -= Time.deltaTime;
-            if (timer <= 0 && ClonePuzzle == true)
-            {
-                timer = 0;
-                vCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 0.7f;
-                vCam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = 0.5f;
-            }
+            Ending.Play(0);
         }
-        else
-            Bridge.transform.localPosition = Vector3.MoveTowards(Bridge.transform.localPosition, GoToBack.transform.localPosition, 1 * Time.deltaTime);
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        ReliableOnTriggerExit.NotifyTriggerEnter(other, gameObject, OnTriggerExit);
-
-        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Clone") || other.gameObject.CompareTag("CubeNormal"))
+        if (other.gameObject.CompareTag("Clone"))
+        {
             activated = true;
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        ReliableOnTriggerExit.NotifyTriggerExit(other, gameObject);
-
-        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Clone") || other.gameObject.CompareTag("CubeNormal"))
-            activated = false;
+            Ending.Play(0);
+        }
     }
 }
