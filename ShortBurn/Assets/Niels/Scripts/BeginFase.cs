71e5a4826c2a96e50b13b135affe267597c4d815
NnelisS <78425965+NnelisS@users.noreply.github.com>
2022-06-21 11:24:27 +0200

    Begingiengie

--- ShortBurn/Assets/Niels/Scripts/BeginFase.cs
+++ ShortBurn/Assets/Niels/Scripts/BeginFase.cs
@@ -8,7 +8,7 @@ public class BeginFase : MonoBehaviour
     public CinemachineVirtualCamera virtualCam;
 
     private bool on = false;
-    private float timer = 23;
+    private float timer = 34;
 
     void Start()
     {
