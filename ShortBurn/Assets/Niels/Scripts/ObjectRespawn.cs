a98c59d8532873665c645b4d56ef6fd46af18d73
Joep-vink <78424962+Joep-vink@users.noreply.github.com>
2022-06-24 09:36:50 +0200

    sounds and ps

--- ShortBurn/Assets/Niels/Scripts/ObjectRespawn.cs
+++ ShortBurn/Assets/Niels/Scripts/ObjectRespawn.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ObjectRespawn : MonoBehaviour
 {
     private Vector3 backPos;
+    public ParticleSystem resetPosition;
 
     void Start()
     {
@@ -21,6 +22,9 @@ public class ObjectRespawn : MonoBehaviour
 
     public void ResetObject()
     {
+        resetPosition.transform.position = transform.position;
+        resetPosition.Play();
+
         transform.position = backPos;
         GetComponent<Rigidbody>().velocity = Vector3.zero;
     }
